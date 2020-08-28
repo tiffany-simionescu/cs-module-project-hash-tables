5ec3c66e315a30a33d586866dbbba30841453d91
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-08-27 23:34:08 -0400

    Day 3-4 - lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,9 @@
 # Your code here
+import math
+import random
 
+# Cache
+cache = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -14,8 +18,14 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    if x + y not in cache:
+        v = math.pow(x,y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+        cache[x + y] = v
 
+    return cache[x + y]    
 
 
 # Do not modify below this line!
